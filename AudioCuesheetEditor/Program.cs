635b14a520feb24b5ee592574d8179a30e4c7926
NeoCoderMatrix86 <40752681+NeoCoderMatrix86@users.noreply.github.com>
2020-12-07 17:08:30 +0100

    added blazorize

--- AudioCuesheetEditor/Program.cs
+++ AudioCuesheetEditor/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +21,13 @@ namespace AudioCuesheetEditor
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBlazorise(options =>
+            {
+                options.ChangeTextOnKeyPress = true;
+            })
+            .AddBootstrapProviders()
+            .AddFontAwesomeIcons();
 
             await builder.Build().RunAsync();
         }
---
+++ AudioCuesheetEditorTests/Model/AudioCuesheet/CuesheetTests.cs
@@ -0,0 +1,36 @@
+﻿//This file is part of AudioCuesheetEditor.
+
+//AudioCuesheetEditor is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//AudioCuesheetEditor is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with Foobar.  If not, see
+//<http: //www.gnu.org/licenses />.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AudioCuesheetEditor.Model.AudioCuesheet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AudioCuesheetEditor.Model.AudioCuesheet.Tests
+{
+    [TestClass()]
+    public class CuesheetTests
+    {
+        [TestMethod()]
+        public void AddTrackTest()
+        {
+            var cuesheet = new Cuesheet();
+            Assert.AreEqual(cuesheet.Tracks.Count, 0);
+            cuesheet.AddTrack(new Track(cuesheet));
+            Assert.AreEqual(cuesheet.Tracks.Count, 1);
+        }
+    }
+}
