99af2d4a4b5a3b0c06ae7e68debe4409872d5a7d
NeoCoderMatrix86 <40752681+NeoCoderMatrix86@users.noreply.github.com>
2020-12-07 12:18:40 +0100

    - added basic code

---
+++ AudioCuesheetEditor/Model/AudioCuesheet/Cuesheet.cs
@@ -0,0 +1,47 @@
+﻿//This file is part of AudioCuesheetEditor.
+
+//AudioCuesheetEditor is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//AudioCuesheetEditor is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with Foobar.  If not, see
+//<http: //www.gnu.org/licenses />.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AudioCuesheetEditor.Model.AudioCuesheet
+{
+    public class Cuesheet
+    {
+        private readonly List<Track> tracks;
+
+        public Cuesheet()
+        {
+            tracks = new List<Track>();
+        }
+
+        public IReadOnlyList<Track> Tracks
+        {
+            get { return tracks.AsReadOnly(); }
+        }
+        public String Artist { get; set; }
+        public String Title { get; set; }
+        public void AddTrack(Track track)
+        {
+            if (track == null)
+            {
+                throw new ArgumentNullException(nameof(track));
+            }
+            tracks.Add(track);
+        }
+    }
+}
---
+++ AudioCuesheetEditor/Model/AudioCuesheet/Track.cs
@@ -0,0 +1,39 @@
+﻿//This file is part of AudioCuesheetEditor.
+
+//AudioCuesheetEditor is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//AudioCuesheetEditor is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with Foobar.  If not, see
+//<http: //www.gnu.org/licenses />.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AudioCuesheetEditor.Model.AudioCuesheet
+{
+    public class Track
+    {
+        public Track(Cuesheet cuesheet)
+        {
+            if (cuesheet == null)
+            {
+                throw new ArgumentNullException(nameof(cuesheet));
+            }
+            Cuesheet = cuesheet;
+        }
+        public Cuesheet Cuesheet { get; private set; }
+        public String Artist { get; set; }
+        public String Title { get; set; }
+        public TimeSpan Begin { get; set; }
+        public TimeSpan End { get; set; }
+    }
+}
