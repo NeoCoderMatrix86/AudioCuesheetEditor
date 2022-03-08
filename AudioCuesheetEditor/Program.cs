2ac4ef44c9f72878496452d54cf80a1f79aa9345
NeoCoderMatrix86 <40752681+NeoCoderMatrix86@users.noreply.github.com>
2022-03-08 16:40:57 +0100

    Migrated to .NET 6.0

--- AudioCuesheetEditor/Program.cs
+++ AudioCuesheetEditor/Program.cs
@@ -1,3 +1,4 @@
+using AudioCuesheetEditor;
 using AudioCuesheetEditor.Controller;
 using AudioCuesheetEditor.Extensions;
 using AudioCuesheetEditor.Model.UI;
@@ -5,62 +6,43 @@ using BlazorDownloadFile;
 using Blazorise;
 using Blazorise.Bootstrap;
 using Howler.Blazor.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
-namespace AudioCuesheetEditor
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazorise(options =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+    options.ChangeTextOnKeyPress = true;
+    options.DelayTextOnKeyPress = true;
+    options.DelayTextOnKeyPressInterval = 300;
+})
+.AddBootstrapProviders();
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IHowl, Howl>();
+builder.Services.AddScoped<IHowlGlobal, HowlGlobal>();
 
-            builder.Services.AddBlazorise(options =>
-            {
-                options.ChangeTextOnKeyPress = true;
-                options.DelayTextOnKeyPress = true;
-                options.DelayTextOnKeyPressInterval = 300;
-            })
-            .AddBootstrapProviders();
+builder.Services.AddBlazorDownloadFile();
 
-            builder.Services.AddScoped<IHowl, Howl>();
-            builder.Services.AddScoped<IHowlGlobal, HowlGlobal>();
+builder.Services.AddScoped<CuesheetController>();
+builder.Services.AddScoped<OptionsController>();
 
-            builder.Services.AddBlazorDownloadFile();
+builder.Services.AddSingleton<SessionStateContainer>();
+builder.Services.AddSingleton<ILoggerProvider, LoggerProvider>();
+builder.Services.AddSingleton<TraceChangeManager>();
 
-            builder.Services.AddScoped<CuesheetController>();
-            builder.Services.AddScoped<OptionsController>();
+builder.Services.AddLogging();
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
-            builder.Services.AddSingleton<SessionStateContainer>();
-            builder.Services.AddSingleton<ILoggerProvider, LoggerProvider>();
-            builder.Services.AddSingleton<TraceChangeManager>();
-            builder.Services.AddLogging();
-            ConfigureLogging(builder);
+builder.Services.AddHotKeys();
 
-            builder.Services.AddHotKeys();
+var host = builder.Build();
 
-            var host = builder.Build();
+await host.SetDefaultCulture();
 
-            await host.SetDefaultCulture();
-
-            await host.RunAsync();
-        }
-
-        private static void ConfigureLogging(WebAssemblyHostBuilder builder, string section = "Logging")
-        {
-            builder.Logging.AddConfiguration(builder.Configuration.GetSection(section));
-        }
-    }
-}
+await builder.Build().RunAsync();
