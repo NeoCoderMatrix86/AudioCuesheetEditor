4999c68ac6b01dcf1401a04566c707bd8958bfaf
NeoCoderMatrix86 <40752681+NeoCoderMatrix86@users.noreply.github.com>
2023-02-06 13:04:16 +0100

    Update Cuesheet.cs

--- AudioCuesheetEditor/Model/AudioCuesheet/Cuesheet.cs
+++ AudioCuesheetEditor/Model/AudioCuesheet/Cuesheet.cs
@@ -507,7 +507,6 @@ namespace AudioCuesheetEditor.Model.AudioCuesheet
                         validationMessages.Add(new ValidationMessage("{0} has no value!", nameof(Audiofile)));
                     }
                     break;
-                //TODO: Validate SplitPoints
             }
             return ValidationResult.Create(validationStatus, validationMessages);
         }
