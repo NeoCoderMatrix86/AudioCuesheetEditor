146f8211815fc12abd2712af7d3684480c779b42
NeoCoderMatrix86 <40752681+NeoCoderMatrix86@users.noreply.github.com>
2023-02-08 15:09:42 +0100

    Update build.sh

--- netlify/build.sh
+++ netlify/build.sh
@@ -2,12 +2,17 @@
 set -e
 
 ## install latest .NET 7.0 release
-
 pushd /tmp
 wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
 chmod u+x /tmp/dotnet-install.sh
 /tmp/dotnet-install.sh --channel 7.0
 popd
+
+## Install wasm-tools
+dotnet workload install wasm-tools
+
+## Run Unit Tests
+dotnet test
 
 ## publish project to known location for subsequent deployment by Netlify
 dotnet publish -c Release -o release
