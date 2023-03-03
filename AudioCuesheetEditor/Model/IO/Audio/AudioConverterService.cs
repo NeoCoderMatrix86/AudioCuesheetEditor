919cbb5f43d1019ee70fd07317899d235fb136e9
NeoCoderMatrix86 <40752681+NeoCoderMatrix86@users.noreply.github.com>
2023-03-03 11:46:22 +0100

    Update AudioConverterService.cs

--- AudioCuesheetEditor/Model/IO/Audio/AudioConverterService.cs
+++ AudioCuesheetEditor/Model/IO/Audio/AudioConverterService.cs
@@ -15,25 +15,33 @@
 //<http: //www.gnu.org/licenses />.
 using FFmpegBlazor;
 using Microsoft.JSInterop;
-using System.Runtime.CompilerServices;
 
 namespace AudioCuesheetEditor.Model.IO.Audio
 {
-    public class AudioConverterService
+    public class AudioConverterService : IDisposable
     {
         private readonly IJSRuntime _jSRuntime;
         private FFMPEG? ffMPEGInstance;
+        private bool disposedValue;
+
+        public event EventHandler<double>? ProgressChanged;
 
         public AudioConverterService(IJSRuntime jSRuntime)
         {
             _jSRuntime = jSRuntime;
         }
 
+        public void Dispose()
+        {
+            // Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode in der Methode "Dispose(bool disposing)" ein.
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
         public Boolean IsReady => (ffMPEGInstance != null) && ffMPEGInstance.IsLoaded;
 
         public async Task<byte[]?> SplitAudiofileAsync(Audiofile audiofile, TimeSpan from, TimeSpan to)
         {
-            //TODO: absichern!
             if (IsReady == false)
             {
                 await InitAsync();
@@ -43,9 +51,6 @@ namespace AudioCuesheetEditor.Model.IO.Audio
                 byte[] buffer = new byte[audiofile.ContentStream.Length];
                 await audiofile.ContentStream.ReadAsync(buffer);
                 ffMPEGInstance.WriteFile(audiofile.Name, buffer);
-                //var newFileName = String.Format("{0}-part1.{1}", Path.GetFileNameWithoutExtension(audiofile.FileName), audiofile.AudioFileType);
-                //ffMPEG.WriteFile($"{newFileName}", buffer);
-                //await ffMPEGInstance.Run("-i", audiofile.Name, "-f", "segment", "-segment_time", "3", "-c", "copy out%03d.mp3");
                 var splitAudiofilename = String.Format("output-{0}{1}", Guid.NewGuid(), audiofile.AudioCodec?.FileExtension);
                 await ffMPEGInstance.Run("-ss", from.ToString("hh\\:mm\\:ss\\.fff"), "-i", audiofile.Name, "-t", to.ToString("hh\\:mm\\:ss\\.fff"), "-c", "copy", splitAudiofilename);
                 ffMPEGInstance.UnlinkFile(audiofile.Name);
@@ -55,13 +60,42 @@ namespace AudioCuesheetEditor.Model.IO.Audio
             return null;
         }
 
-        private async Task InitAsync()
+        public async Task InitAsync()
         {
             //initialize FFmpegFactory
             await FFmpegFactory.Init(_jSRuntime);
+            if (ffMPEGInstance == null)
+            {
+                ffMPEGInstance = FFmpegFactory.CreateFFmpeg(new FFmpegConfig() { Log = true });
+                await ffMPEGInstance.Load(true);
 
-            ffMPEGInstance = FFmpegFactory.CreateFFmpeg(new FFmpegConfig() { Log = true });
-            await ffMPEGInstance.Load();
+                ffMPEGInstance.Progress += FFMPEGInstance_Progress;
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Verwalteten Zustand (verwaltete Objekte) bereinigen
+                    if (ffMPEGInstance != null)
+                    {
+                        ffMPEGInstance.Progress -= FFMPEGInstance_Progress;
+                    }
+                    
+                }
+
+                // Nicht verwaltete Ressourcen (nicht verwaltete Objekte) freigeben und Finalizer überschreiben
+                // Große Felder auf NULL setzen
+                disposedValue = true;
+            }
+        }
+
+        private void FFMPEGInstance_Progress(Progress p)
+        {
+            ProgressChanged?.Invoke(this, p.Ratio);
         }
     }
 }
