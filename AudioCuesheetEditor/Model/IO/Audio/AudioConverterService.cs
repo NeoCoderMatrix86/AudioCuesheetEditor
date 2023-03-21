851428ee841dfc882edaecd4c8a6ec3552fbb95e
NeoCoderMatrix86 <40752681+NeoCoderMatrix86@users.noreply.github.com>
2023-03-21 09:09:10 +0100

    Update AudioConverterService.cs

--- AudioCuesheetEditor/Model/IO/Audio/AudioConverterService.cs
+++ AudioCuesheetEditor/Model/IO/Audio/AudioConverterService.cs
@@ -13,7 +13,6 @@
 //You should have received a copy of the GNU General Public License
 //along with Foobar.  If not, see
 //<http: //www.gnu.org/licenses />.
-using ATL.CatalogDataReaders.BinaryLogic;
 using FFmpegBlazor;
 using Microsoft.JSInterop;
 
