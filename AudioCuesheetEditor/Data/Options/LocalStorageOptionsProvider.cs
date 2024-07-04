19e153f9d67ee7fc12b44c8a21ef0c456fbfa1fc
NeoCoderMatrix86 <40752681+NeoCoderMatrix86@users.noreply.github.com>
2024-07-04 08:43:02 +0200

    Added saving of a single option

--- AudioCuesheetEditor/Data/Options/LocalStorageOptionsProvider.cs
+++ AudioCuesheetEditor/Data/Options/LocalStorageOptionsProvider.cs
@@ -13,8 +13,11 @@
 //You should have received a copy of the GNU General Public License
 //along with Foobar.  If not, see
 //<http: //www.gnu.org/licenses />.
+using AudioCuesheetEditor.Model.Entity;
 using AudioCuesheetEditor.Model.Options;
 using Microsoft.JSInterop;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text.Json;
 
 namespace AudioCuesheetEditor.Data.Options
@@ -63,6 +66,48 @@ namespace AudioCuesheetEditor.Data.Options
             var optionsJson = JsonSerializer.Serialize<object>(options, SerializerOptions);
             await _jsRuntime.InvokeVoidAsync(String.Format("{0}.set", options.GetType().Name), optionsJson);
             OptionSaved?.Invoke(this, options);
+        }
+
+        public async Task SaveOptionsValue<T>(Expression<Func<T, object>> propertyExpression, object value) where T : class, IOptions, new()
+        {
+            var options = await GetOptions<T>();
+            if (propertyExpression.Body is MemberExpression memberExpression)
+            {
+                var propertyInfo = memberExpression.Member as PropertyInfo;
+                if (propertyInfo != null)
+                {
+                    propertyInfo.SetValue(options, Convert.ChangeType(value, propertyInfo.PropertyType));
+                }
+                else
+                {
+                    throw new ArgumentException("The provided expression does not reference a valid property.");
+                }
+            }
+            else if (propertyExpression.Body is UnaryExpression unaryExpression && unaryExpression.Operand is MemberExpression unaryMemberExpression)
+            {
+                var propertyInfo = unaryMemberExpression.Member as PropertyInfo;
+                if (propertyInfo != null)
+                {
+                    propertyInfo.SetValue(options, Convert.ChangeType(value, propertyInfo.PropertyType));
+                }
+                else
+                {
+                    throw new ArgumentException("The provided expression does not reference a valid property.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("The provided expression does not reference a valid property.");
+            }
+            Boolean saveOptions = true;
+            if (options is IValidateable<T> validateable)
+            {
+                saveOptions = validateable.Validate(propertyExpression).Status != ValidationStatus.Error;
+            }
+            if (saveOptions)
+            {
+                await SaveOptions(options);
+            }
         }
     }
 }
