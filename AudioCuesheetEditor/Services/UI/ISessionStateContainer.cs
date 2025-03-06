dfc7f92da83a944d15495ad642783fb140574e5b
Sven <40752681+NeoCoderMatrix86@users.noreply.github.com>
2025-03-06 14:51:31 +0100

    Update ISessionStateContainer.cs

--- AudioCuesheetEditor/Services/UI/ISessionStateContainer.cs
+++ AudioCuesheetEditor/Services/UI/ISessionStateContainer.cs
@@ -1,4 +1,19 @@
-﻿using AudioCuesheetEditor.Model.AudioCuesheet;
+﻿//This file is part of AudioCuesheetEditor.
+
+//AudioCuesheetEditor is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//AudioCuesheetEditor is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with Foobar.  If not, see
+//<http: //www.gnu.org/licenses />.
+using AudioCuesheetEditor.Model.AudioCuesheet;
 using AudioCuesheetEditor.Model.IO.Audio;
 using AudioCuesheetEditor.Model.IO.Import;
 
