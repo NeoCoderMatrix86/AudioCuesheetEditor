0eac04db6384f8050035fc29d1aba653a2b32159
Sven <40752681+NeoCoderMatrix86@users.noreply.github.com>
2025-05-19 15:18:15 +0200

    First tests added

---
+++ AudioCuesheetEditor.End2EndTests/Pages/IndexTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+
+namespace AudioCuesheetEditor.End2EndTests.Pages
+{
+    [TestClass]
+    public class IndexTest : PageTest
+    {
+        [TestMethod]
+        public async Task HasTitle()
+        {
+            await Page.GotoAsync("https://localhost:7132/");
+            await Expect(Page).ToHaveTitleAsync("AudioCuesheetEditor");
+            await Expect(Page.GetByRole(AriaRole.Button, new() { Name = "AudioCuesheetEditor" })).ToBeVisibleAsync();
+        }
+    }
+}
