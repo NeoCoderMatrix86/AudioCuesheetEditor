9b5ac727f46b4574b3472f065e6571c4cd4da2a3
Sven <40752681+NeoCoderMatrix86@users.noreply.github.com>
2025-05-20 10:08:43 +0200

    use trace in case of failed test

--- AudioCuesheetEditor.End2EndTests/Pages/AboutTest.cs
+++ AudioCuesheetEditor.End2EndTests/Pages/AboutTest.cs
@@ -6,6 +6,33 @@ namespace AudioCuesheetEditor.End2EndTests.Pages
     [TestClass]
     public class AboutTest : PageTest
     {
+        [TestInitialize]
+        public async Task TestInitialize()
+        {
+            await Context.Tracing.StartAsync(new()
+            {
+                Title = $"{TestContext.FullyQualifiedTestClassName}.{TestContext.TestName}",
+                Screenshots = true,
+                Snapshots = true,
+                Sources = true
+            });
+        }
+
+        [TestCleanup]
+        public async Task TestCleanup()
+        {
+            var failed = new[] { UnitTestOutcome.Failed, UnitTestOutcome.Error, UnitTestOutcome.Timeout, UnitTestOutcome.Aborted }.Contains(TestContext.CurrentTestOutcome);
+
+            await Context.Tracing.StopAsync(new()
+            {
+                Path = failed ? Path.Combine(
+                    Environment.CurrentDirectory,
+                    "playwright-traces",
+                    $"{TestContext.FullyQualifiedTestClassName}.{TestContext.TestName}.zip"
+                ) : null,
+            });
+        }
+
         [TestMethod]
         public async Task HasTitle()
         {
--- AudioCuesheetEditor.End2EndTests/Pages/IndexTest.cs
+++ AudioCuesheetEditor.End2EndTests/Pages/IndexTest.cs
@@ -1,11 +1,39 @@
 ﻿using Microsoft.Playwright;
 using Microsoft.Playwright.MSTest;
+using System.Text.RegularExpressions;
 
 namespace AudioCuesheetEditor.End2EndTests.Pages
 {
     [TestClass]
     public class IndexTest : PageTest
     {
+        [TestInitialize]
+        public async Task TestInitialize()
+        {
+            await Context.Tracing.StartAsync(new()
+            {
+                Title = $"{TestContext.FullyQualifiedTestClassName}.{TestContext.TestName}",
+                Screenshots = true,
+                Snapshots = true,
+                Sources = true
+            });
+        }
+
+        [TestCleanup]
+        public async Task TestCleanup()
+        {
+            var failed = new[] { UnitTestOutcome.Failed, UnitTestOutcome.Error, UnitTestOutcome.Timeout, UnitTestOutcome.Aborted }.Contains(TestContext.CurrentTestOutcome);
+
+            await Context.Tracing.StopAsync(new()
+            {
+                Path = failed ? Path.Combine(
+                    Environment.CurrentDirectory,
+                    "playwright-traces",
+                    $"{TestContext.FullyQualifiedTestClassName}.{TestContext.TestName}.zip"
+                ) : null,
+            });
+        }
+
         [TestMethod]
         public async Task HasTitle()
         {
@@ -14,9 +42,16 @@ namespace AudioCuesheetEditor.End2EndTests.Pages
             await Expect(Page.GetByRole(AriaRole.Button, new() { Name = "AudioCuesheetEditor" })).ToBeVisibleAsync();
         }
 
+        [TestMethod]
+        public async Task CheckSettings()
+        {
+            await Page.GotoAsync("http://localhost:5132/");
+            await Page.GetByRole(AriaRole.Toolbar).GetByRole(AriaRole.Button).Filter(new() { HasTextRegex = new Regex("^$") }).Nth(3).ClickAsync();
+            await Page.Locator("div").Filter(new() { HasTextRegex = new Regex("^Settings$") }).ClickAsync();
+            await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Settings" })).ToBeVisibleAsync();
+        }
         //TODO: Change language test
         //TODO: Switch view test
-        //TODO: Settings test
         //TODO: Import sample cuesheet test
     }
 }
