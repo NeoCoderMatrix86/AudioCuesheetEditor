8723d0d12375e00f752821c79b1a35b57d80522f
Sven <40752681+NeoCoderMatrix86@users.noreply.github.com>
2025-05-22 14:56:10 +0200

    always write trace

--- AudioCuesheetEditor.End2EndTests/Pages/IndexTest.cs
+++ AudioCuesheetEditor.End2EndTests/Pages/IndexTest.cs
@@ -22,15 +22,13 @@ namespace AudioCuesheetEditor.End2EndTests.Pages
         [TestCleanup]
         public async Task TestCleanup()
         {
-            var failed = new[] { UnitTestOutcome.Failed, UnitTestOutcome.Error, UnitTestOutcome.Timeout, UnitTestOutcome.Aborted }.Contains(TestContext.CurrentTestOutcome);
-
             await Context.Tracing.StopAsync(new()
             {
-                Path = failed ? Path.Combine(
+                Path = Path.Combine(
                     Environment.CurrentDirectory,
                     "playwright-traces",
                     $"{TestContext.FullyQualifiedTestClassName}.{TestContext.TestName}.zip"
-                ) : null,
+                )
             });
         }
 
@@ -76,18 +74,17 @@ namespace AudioCuesheetEditor.End2EndTests.Pages
             await Expect(Page.GetByRole(AriaRole.Cell, new() { Name = "Test Track 2 Title Clear" })).ToBeVisibleAsync();
         }
 
-        //TODO
-        //[TestMethod]
-        //public async Task ChangeLanguage()
-        //{
-        //    // We need to use a new context because the default one doesn't work with changing a language
-        //    var context = await Browser.NewContextAsync();
-        //    var page = await context.NewPageAsync();
-        //    await page.GotoAsync("http://localhost:5132/");
-        //    await page.GetByRole(AriaRole.Button, new() { Name = "Change language" }).ClickAsync();
-        //    await page.Locator("div").Filter(new() { HasTextRegex = new Regex("^German \\(Germany\\)$") }).ClickAsync();
-        //    await Expect(page.Locator("#app")).ToContainTextAsync("Allgemeine Informationen");
-        //}
+        [TestMethod]
+        public async Task ChangeLanguage()
+        {
+            // We need to use a new context because the default one doesn't work with changing a language
+            var context = await Browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+            await page.GotoAsync("http://localhost:5132/");
+            await page.GetByRole(AriaRole.Button, new() { Name = "Change language" }).ClickAsync();
+            await page.Locator("div").Filter(new() { HasTextRegex = new Regex("^German \\(Germany\\)$") }).ClickAsync();
+            await Expect(page.Locator("#app")).ToContainTextAsync("Allgemeine Informationen");
+        }
 
         [TestMethod]
         public async Task Import()
@@ -104,66 +101,65 @@ namespace AudioCuesheetEditor.End2EndTests.Pages
             await Expect(Page.GetByRole(AriaRole.Group).Filter(new() { HasText = "AudiofileAudiofile Search" }).Locator("input[type=\"file\"]")).ToBeEmptyAsync();
         }
 
-        //TODO
-//        [TestMethod]
-//        public async Task GenerateCuesheet()
-//        {
-//            await Page.GotoAsync("http://localhost:5132/");
-//            await Page.GetByRole(AriaRole.Button, new() { Name = "File", Exact = true }).ClickAsync();
-//            await Page.GetByText("Open").ClickAsync();
-//            await Page.Locator("#dropFileInputId_SelectFileDialog").GetByRole(AriaRole.Button, new() { Name = "Choose File" }).ClickAsync();
-//            await Page.Locator("#dropFileInputId_SelectFileDialog").GetByRole(AriaRole.Button, new() { Name = "Choose File" }).SetInputFilesAsync(new[] { "../../../../AudioCuesheetEditor/wwwroot/samples/Sample_Cuesheet.cue" });
-//            await Expect(Page.GetByRole(AriaRole.Textbox, new() { Name = "Cuesheet artist" })).ToHaveValueAsync("Sample CD Artist");
-//            await Expect(Page.GetByRole(AriaRole.Textbox, new() { Name = "Cuesheet title" })).ToHaveValueAsync("Sample CD Title");
-//            await Expect(Page.GetByRole(AriaRole.Cell, new() { Name = "Sample Artist 6" }).GetByRole(AriaRole.Textbox)).ToHaveValueAsync("Sample Artist 6");
-//            await Expect(Page.GetByRole(AriaRole.Row, new() { Name = "Increment Decrement Sample Artist 7 Clear Sample Title 7 Clear 00:31:54 00:45:" }).GetByRole(AriaRole.Textbox).Nth(3)).ToHaveValueAsync("00:45:54");
-//            await Page.GetByRole(AriaRole.Button, new() { Name = "File", Exact = true }).ClickAsync();
-//            await Page.GetByText("Export").ClickAsync();
-//            await Page.GetByText("Cuesheet", new() { Exact = true }).ClickAsync();
-//            var download = await Page.RunAndWaitForDownloadAsync(async () =>
-//            {
-//                await Page.GetByRole(AriaRole.Row, new() { Name = "Cuesheet.cue 00:00:00 00:45:" }).GetByRole(AriaRole.Button).ClickAsync();
-//            });
-//            // Read the downloaded file content as text
-//            var cuesheetContent = await download.PathAsync();
-//            var actualText = await File.ReadAllTextAsync(cuesheetContent);
-//            var expectedText = @"TITLE ""Sample CD Title""
-//PERFORMER ""Sample CD Artist""
-//FILE ""Sample.mp3"" MP3
-//	TRACK 01 AUDIO
-//		TITLE ""Sample Title 1""
-//		PERFORMER ""Sample Artist 1""
-//		INDEX 01 00:00:00
-//	TRACK 02 AUDIO
-//		TITLE ""Sample Title 2""
-//		PERFORMER ""Sample Artist 2""
-//		INDEX 01 05:00:00
-//	TRACK 03 AUDIO
-//		TITLE ""Sample Title 3""
-//		PERFORMER ""Sample Artist 3""
-//		INDEX 01 09:23:00
-//	TRACK 04 AUDIO
-//		TITLE ""Sample Title 4""
-//		PERFORMER ""Sample Artist 4""
-//		INDEX 01 15:54:00
-//	TRACK 05 AUDIO
-//		TITLE ""Sample Title 5""
-//		PERFORMER ""Sample Artist 5""
-//		INDEX 01 20:13:00
-//	TRACK 06 AUDIO
-//		TITLE ""Sample Title 6""
-//		PERFORMER ""Sample Artist 6""
-//		INDEX 01 24:54:00
-//	TRACK 07 AUDIO
-//		TITLE ""Sample Title 7""
-//		PERFORMER ""Sample Artist 7""
-//		INDEX 01 31:54:00
-//	TRACK 08 AUDIO
-//		TITLE ""Sample Title 8""
-//		PERFORMER ""Sample Artist 8""
-//		INDEX 01 45:54:00
-//";
-//            Assert.AreEqual(expectedText.Replace("\r\n", "\n"), actualText.Replace("\r\n", "\n"));
-//        }
+        [TestMethod]
+        public async Task GenerateCuesheet()
+        {
+            await Page.GotoAsync("http://localhost:5132/");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "File", Exact = true }).ClickAsync();
+            await Page.GetByText("Open").ClickAsync();
+            await Page.Locator("#dropFileInputId_SelectFileDialog").GetByRole(AriaRole.Button, new() { Name = "Choose File" }).ClickAsync();
+            await Page.Locator("#dropFileInputId_SelectFileDialog").GetByRole(AriaRole.Button, new() { Name = "Choose File" }).SetInputFilesAsync(new[] { "../../../../AudioCuesheetEditor/wwwroot/samples/Sample_Cuesheet.cue" });
+            await Expect(Page.GetByRole(AriaRole.Textbox, new() { Name = "Cuesheet artist" })).ToHaveValueAsync("Sample CD Artist");
+            await Expect(Page.GetByRole(AriaRole.Textbox, new() { Name = "Cuesheet title" })).ToHaveValueAsync("Sample CD Title");
+            await Expect(Page.GetByRole(AriaRole.Cell, new() { Name = "Sample Artist 6" }).GetByRole(AriaRole.Textbox)).ToHaveValueAsync("Sample Artist 6");
+            await Expect(Page.GetByRole(AriaRole.Row, new() { Name = "Increment Decrement Sample Artist 7 Clear Sample Title 7 Clear 00:31:54 00:45:" }).GetByRole(AriaRole.Textbox).Nth(3)).ToHaveValueAsync("00:45:54");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "File", Exact = true }).ClickAsync();
+            await Page.GetByText("Export").ClickAsync();
+            await Page.GetByText("Cuesheet", new() { Exact = true }).ClickAsync();
+            var download = await Page.RunAndWaitForDownloadAsync(async () =>
+            {
+                await Page.GetByRole(AriaRole.Row, new() { Name = "Cuesheet.cue 00:00:00 00:45:" }).GetByRole(AriaRole.Button).ClickAsync();
+            });
+            // Read the downloaded file content as text
+            var cuesheetContent = await download.PathAsync();
+            var actualText = await File.ReadAllTextAsync(cuesheetContent);
+            var expectedText = @"TITLE ""Sample CD Title""
+PERFORMER ""Sample CD Artist""
+FILE ""Sample.mp3"" MP3
+	TRACK 01 AUDIO
+		TITLE ""Sample Title 1""
+		PERFORMER ""Sample Artist 1""
+		INDEX 01 00:00:00
+	TRACK 02 AUDIO
+		TITLE ""Sample Title 2""
+		PERFORMER ""Sample Artist 2""
+		INDEX 01 05:00:00
+	TRACK 03 AUDIO
+		TITLE ""Sample Title 3""
+		PERFORMER ""Sample Artist 3""
+		INDEX 01 09:23:00
+	TRACK 04 AUDIO
+		TITLE ""Sample Title 4""
+		PERFORMER ""Sample Artist 4""
+		INDEX 01 15:54:00
+	TRACK 05 AUDIO
+		TITLE ""Sample Title 5""
+		PERFORMER ""Sample Artist 5""
+		INDEX 01 20:13:00
+	TRACK 06 AUDIO
+		TITLE ""Sample Title 6""
+		PERFORMER ""Sample Artist 6""
+		INDEX 01 24:54:00
+	TRACK 07 AUDIO
+		TITLE ""Sample Title 7""
+		PERFORMER ""Sample Artist 7""
+		INDEX 01 31:54:00
+	TRACK 08 AUDIO
+		TITLE ""Sample Title 8""
+		PERFORMER ""Sample Artist 8""
+		INDEX 01 45:54:00
+";
+            Assert.AreEqual(expectedText.Replace("\r\n", "\n"), actualText.Replace("\r\n", "\n"));
+        }
     }
 }
