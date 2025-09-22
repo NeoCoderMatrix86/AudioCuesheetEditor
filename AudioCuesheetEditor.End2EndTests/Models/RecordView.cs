d7899e6ff47334c1d1c7f09e46fb9fae79f1223d
Sven <40752681+NeoCoderMatrix86@users.noreply.github.com>
2025-09-22 16:10:55 +0200

    refactor recording tests

---
+++ AudioCuesheetEditor.End2EndTests/Models/RecordView.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Playwright;
+
+namespace AudioCuesheetEditor.End2EndTests.Models
+{
+    public class RecordView(IPage page)
+    {
+        public const string BaseUrl = "http://localhost:5132/";
+
+        private readonly IPage _page = page;
+
+        public async Task GotoAsync()
+        {
+            await _page.GotoAsync(BaseUrl);
+            await _page.WaitForURLAsync(BaseUrl);
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        }
+
+        public async Task StartRecordingAsync()
+        {
+            await _page.GetByText("Record view").ClickAsync();
+            await _page.GetByRole(AriaRole.Button, new() { Name = "Start recording" }).ClickAsync();
+        }
+
+        public async Task StopRecordingAsync()
+        {
+            await _page.GetByRole(AriaRole.Button, new() { Name = "Stop recording" }).ClickAsync();
+        }
+
+        public async Task AddRecordingTrackAsync(string artist, string title)
+        {
+            await _page.GetByRole(AriaRole.Textbox, new() { Name = "Artist", Exact = true }).ClickAsync();
+            await _page.GetByRole(AriaRole.Textbox, new() { Name = "Artist", Exact = true }).FillAsync(artist);
+            await _page.GetByRole(AriaRole.Textbox, new() { Name = "Artist", Exact = true }).PressAsync("Tab");
+            await _page.GetByRole(AriaRole.Textbox, new() { Name = "Title", Exact = true }).FillAsync(title);
+            await _page.GetByRole(AriaRole.Textbox, new() { Name = "Title", Exact = true }).PressAsync("Tab");
+            await _page.GetByRole(AriaRole.Button, new() { Name = "Add track" }).ClickAsync();
+            await _page.Locator(".mud-overlay").ClickAsync();
+        }
+    }
+}
--- AudioCuesheetEditor.End2EndTests/Tests/RecordTest.cs
+++ AudioCuesheetEditor.End2EndTests/Tests/RecordTest.cs
@@ -9,12 +9,12 @@ namespace AudioCuesheetEditor.End2EndTests.Tests
         [TestMethod]
         public async Task RecordAsync()
         {
-            var page = new IndexPage(TestPage);
-            await page.GotoAsync();
-            await page.StartRecordingAsync();
-            await page.AddRecordingTrackAsync("Test Track 1 Artist", "Test Track 1 Title");
-            await page.AddRecordingTrackAsync("Test Track 2 Artist", "Test Track 2 Title");
-            await page.StopRecordingAsync();
+            var recordView = new RecordView(TestPage);
+            await recordView.GotoAsync();
+            await recordView.StartRecordingAsync();
+            await recordView.AddRecordingTrackAsync("Test Track 1 Artist", "Test Track 1 Title");
+            await recordView.AddRecordingTrackAsync("Test Track 2 Artist", "Test Track 2 Title");
+            await recordView.StopRecordingAsync();
             await Expect(TestPage.GetByRole(AriaRole.Cell, new() { Name = "Test Track 1 Artist Clear" })).ToBeVisibleAsync();
             await Expect(TestPage.GetByRole(AriaRole.Cell, new() { Name = "Test Track 1 Title Clear" })).ToBeVisibleAsync();
             await Expect(TestPage.GetByRole(AriaRole.Cell, new() { Name = "Test Track 2 Artist Clear" })).ToBeVisibleAsync();
