060d042960cf7b60185f567fcd7f0d790ce864ed
Sven <40752681+NeoCoderMatrix86@users.noreply.github.com>
2025-09-26 11:15:21 +0200

    Create LocalizationTestSmartphone.cs

---
+++ AudioCuesheetEditor.End2EndTests/Tests/Smartphone/LocalizationTestSmartphone.cs
@@ -0,0 +1,22 @@
+﻿using AudioCuesheetEditor.End2EndTests.Models;
+using Microsoft.Playwright;
+
+namespace AudioCuesheetEditor.End2EndTests.Tests.Smartphone
+{
+    [TestClass]
+    public class LocalizationTestSmartphone : PlaywrightTestBase
+    {
+        [TestMethod]
+        public async Task ChangeLanguage_ShouldShowGermanHeadings_WhenGermanIsSelected()
+        {
+            var bar = new AppBar(TestPage);
+            await bar.GotoAsync();
+            await bar.ChangeLanguageAsync("German (Germany)");
+            await Expect(TestPage.GetByRole(AriaRole.Heading, new() { Name = "Abschnitte" })).ToBeVisibleAsync();
+            await Expect(TestPage.GetByRole(AriaRole.Heading, new() { Name = "Allgemeine Informationen" })).ToBeVisibleAsync();
+            await Expect(TestPage.GetByText("Aufnahmeansicht")).ToBeVisibleAsync();
+            await Expect(TestPage.GetByRole(AriaRole.Heading, new() { Name = "Titel" })).ToBeVisibleAsync();
+            await Expect(TestPage.GetByRole(AriaRole.Heading, new() { Name = "Wiedergabe" })).ToBeVisibleAsync();
+        }
+    }
+}
